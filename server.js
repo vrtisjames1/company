09dcf8ffce9992fb7e3f76e6819896ab20d4f898
James Vrtis <jamesvrtis@caskradmins-mbp.mynetworksettings.com>
2022-11-09 16:25:41 -0500

    update

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ if(process.env.PORT){
 	PORT = process.env.PORT
 }
 
-app.get('/', (req, res)=>{
-	res.send('hi');
-})
+// app.get('/', (req, res)=>{
+// 	res.send('hi');
+// })
 
 app.listen(PORT, ()=>{
 	console.log('listening');
